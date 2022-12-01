60a760a0233e1513e4d410af55bb7d0418b4453c
ShodmonovZafar <uzbekdasturchisiman@gmail.com>
2022-12-01 10:18:04 +0500

    _

--- json_1.py
+++ json_1.py
@@ -1,8 +1,21 @@
 import json
-d = {
+
+data_1 = {}
+
+for i in range(1, 13):
+    data_1[i] = 0
+    
+data_2 = {
     'foo': 'bar',
     'alice': 1,
     'wonderland': [1, 2, 3]
 }
-with open("file_name.txt", 'w') as f:
-    json.dump(d, f)
+
+path_to_the_json_file_1 = "json_file/json_file_1.json"
+path_to_the_json_file_2 = "json_file/json_file_2.json"
+
+with open(path_to_the_json_file_1, 'w') as f:
+    json.dump(data_1, f)
+
+with open(path_to_the_json_file_2, 'w') as f:
+    json.dump(data_2, f)
--- json_2.py
+++ json_2.py
@@ -1,3 +1,13 @@
 import json
-with open("file_name.txt", 'r') as f:
-    d = json.load(f)
+
+path_to_the_json_file_1 = "json_file/json_file_1.json"
+path_to_the_json_file_2 = "json_file/json_file_2.json"
+
+with open(path_to_the_json_file_1, 'r') as f:
+    data_1 = json.load(f)
+
+with open(path_to_the_json_file_2, 'r') as f:
+    data_2 = json.load(f)
+
+print(data_1)
+print(data_2)
